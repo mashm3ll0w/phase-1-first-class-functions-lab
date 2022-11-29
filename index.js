562e13a148593c3e5f7418dc1e8383f81c36c788
Charles Swaleh <charlswall@live.com>
2022-11-29 13:46:08 +0300

    Completed advanced function: first class functions lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare){
+  return function(){
+    return fare * fare
+  }
+}
+
+const fareToDouble = createFareMultiplier;
+
+function fareDoubler(fareToDouble){
+  return fareToDouble * 2;
+}
+
+const fareToTriple = createFareMultiplier;
+
+function fareTripler(fareToTriple){
+  return fareToTriple * 3;
+}
+
+function selectDifferentDrivers(drivers, fn){
+  return fn(drivers);
+}
